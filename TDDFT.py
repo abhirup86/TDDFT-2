b884c65681d662df1f3b8a28e933a60357dfadfd
Yaroslav Zhumagulov <yaroslav.zhumagulov@gmail.com>
2019-02-25 15:46:19 +0300

    update Test

--- TDDFT.py
+++ TDDFT.py
@@ -19,40 +19,40 @@ def operator_matrix_periodic(matrix,operator,wf_conj,wf):
 
 @numba.jit(nopython=True,parallel=True,fastmath=True)
 def Fock_matrix(matrix,V,M_conj,M,occupation,ibz_map):
-    """perform integration of periodic part of Kohn Sham wavefunction"""
+    """
+    perform integration for Fock matrix
+    M - pair-density matrices <m|exp(1j*(q+G))|n>
+    V - coloumb potential V(q+G)
+    """
     nbands=matrix.shape[1];NK=V.shape[0];NKF=V.shape[1]
     for q in numba.prange(NKF):
         for m in range(nbands):
-            for k in range(NK):
-                for n1 in range(nbands):
-                    for n2 in range(nbands):
-                        matrix[k,n1,n2]+=np.sum(occupation[ibz_map[q],m]*V[k,q]*M_conj[m,n1,ibz_map[q],k]*M[m,n2,ibz_map[q],k])
+            if occupation[ibz_map[q],m]>1e-8:
+                for k in range(NK):
+                    for n1 in range(nbands):
+                        for n2 in range(nbands):
+                            matrix[k,n1,n2]+=np.sum(occupation[ibz_map[q],m]*V[k,q]*M_conj[m,n1,ibz_map[q],k]*M[m,n2,ibz_map[q],k])
     matrix/=NKF
     return matrix
 
 class TDDFT(object):
     """
-    Time-dependent Hartree-Fock:
+    Time-dependent DFT+Hartree-Fock in Kohn-Sham orbitals basis:
     
-        calc: GPAW calculator (symmetry='off' is preferable)
-        nbands (int): number of unoccupied bands in calculation
+        calc: GPAW calculator (setups='sg15')
+        nbands (int): number of bands in calculation
         
     """
     
-    def __init__(self,calc,nbands=None):
+    def __init__(self,calc):
         self.calc=calc # GPAW calculator object
         self.K=calc.get_ibz_k_points() # reduced Brillioun zone
         self.NK=self.K.shape[0] 
         
         self.wk=calc.get_k_point_weights() # weight of reduced Brillioun zone
-        
-       
-        
+    
         self.nvalence=int(calc.occupations.nvalence/2) #number of valence bands
-        if nbands is None:
-            self.nbands=2*self.nvalence
-        else:
-            self.nbands=nbands+self.nvalence
+        self.nbands=calc.get_number_of_bands()
         
         self.EK=[calc.get_eigenvalues(k)[:self.nbands] for k in range(self.NK)] # bands energy
         self.EK=np.array(self.EK)/Hartree
@@ -88,7 +88,9 @@ class TDDFT(object):
         self.temperature=calc.occupations.width
         
         #calculate pair-density matrices
-        self.M=np.zeros((self.nbands,self.nbands,self.NK,self.NK,self.pdF.get_reciprocal_vectors().shape[0]),dtype=np.complex)
+        self.M=np.zeros((self.nbands,self.nbands,
+                         self.NK,self.NK,
+                         self.pdF.get_reciprocal_vectors().shape[0]),dtype=np.complex)
         indexes=[(n,k) for n,k in product(range(self.nbands),range(self.NK))]
         for i1 in range(len(indexes)):
             n1,k1=indexes[i1]
