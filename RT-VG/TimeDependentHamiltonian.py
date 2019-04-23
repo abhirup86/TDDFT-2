98485a1333d612fd410b4f1067d9b4309df48858
yaros72 <yaroslav.zhumagulov@gmail.com>
2019-04-23 20:57:19 +0300

    update local potential

--- RT-VG/TimeDependentHamiltonian.py
+++ RT-VG/TimeDependentHamiltonian.py
@@ -2,7 +2,7 @@ import numpy as np
 from gpaw.wavefunctions.pw import PWLFC
 from gpaw.utilities import unpack
 from gpaw.transformers import Transformer
-import xc 
+import pylibxc
 
 class TimeDependentHamiltonian():
     
@@ -52,7 +52,7 @@ class TimeDependentHamiltonian():
             psi=[];den=[]
             for n in range(self.nbands):
                 psi.append(self.pd.ifft(kpt.psit_nG[n],kpt.q))
-                den.append(self.pd.ifft(np.abs(kpt.psit_nG[n])**2,kpt.q))
+                den.append(np.abs(psi[-1])**2)
                 for m in range(self.nbands):
                     self.kinetic[kpt.q,n,m]=self.pd.integrate(kpt.psit_nG[n],kinetic*kpt.psit_nG[m])
                     
@@ -65,7 +65,7 @@ class TimeDependentHamiltonian():
             den_gs.append(den)
          
         self.psi_gs=np.array(psi_gs)
-        self.den_gs=np.array(den_gs)
+        self.den_gs=np.array(den_gs,dtype=float)
         
         #scalar product with vector potential direction
         self.interaction_momentum=np.einsum('iqnm,i->qnm',self.momentum,direction)
@@ -75,6 +75,7 @@ class TimeDependentHamiltonian():
         
         #initialization local and nonlocal part of pseudopotential
         self.init_potential()
+        self.density=self.den.nt_sG.copy()
         
         #initialization charge density (ion+electrons) for Hartree potential
         self.ion_density=calc.hamiltonian.poisson.pd.ifft(calc.density.rhot_q)-calc.density.nt_sg[0]
@@ -82,30 +83,33 @@ class TimeDependentHamiltonian():
         self.pd0=self.ham.poisson.pd
         #reciprocal |G|^2 vectors for Hartree potential V(G)=4pi/|G|^2
         self.G2=self.ham.poisson.G2_q
+        self.G=self.pd0.get_reciprocal_vectors()
         
         #fine to coarse and coarse to fine grids transformers (for correct calculation local potential)
         self.fine_to_coarse=Transformer(calc.density.finegd, calc.density.gd, 3) 
         self.coarse_to_fine=Transformer(calc.density.gd, calc.density.finegd, 3)
         
         #initialization local potenital from ground state density
-        self.update_local_potential(calc.density.nt_sG[0])
+        self.update_local_potential()
         
         
     
-    def update_local_potential(self,density):
+    def update_local_potential(self):
         
         #tranform density from coarse to fine grids
-        density=self.coarse_to_fine.apply(density)
+        density=self.coarse_to_fine.apply(self.density.copy())
         
-        #calculte XC - potential
-        VXC=xc.VLDAx(density)+xc.VLDAc(density)
+        #calculate XC potential
+        VXC=np.zeros_like(density)
+        self.ham.xc.calculate(self.pd0.gd,density,VXC)
+          
         #calculate Hartree potential
         charge_density=density+self.ion_density
         VH=4*np.pi*self.pd0.fft(charge_density)/self.G2
         VH=self.pd0.ifft(VH)
         
         #transform Hartree and XC potential from fine to coarse grids
-        self.VXC=self.fine_to_coarse.apply(VXC)
+        self.VXC=self.fine_to_coarse.apply(VXC[0])
         self.VH=self.fine_to_coarse.apply(VH)
             
         
@@ -147,13 +151,12 @@ class TimeDependentHamiltonian():
         self.norb=self.V.size # number of orbitals in nonlocal potential
         
         #initialization of local part of pseudopotential
-        V=calc.hamiltonian.vbar.pd.zeros()
-        calc.hamiltonian.vbar.add(V)
-        self.Vloc=calc.hamiltonian.vbar.pd.ifft(V)
+        V=self.ham.vbar.pd.zeros()
+        self.ham.vbar.add(V)
+        self.Vloc=self.ham.vbar.pd.ifft(V)
         
-    def calculate_density(self,wfn):
-        #calculation of density
-        return np.einsum('qn,qmn,qmxyz->xyz',self.f_n,np.abs(wfn)**2,self.den_gs)
+    def update_density(self,wfn):
+        self.density[0]=np.einsum('qn,qmn,qmxyz->xyz',self.f_n,np.abs(wfn)**2,self.den_gs)
     
     def calculate_nonlocal(self,A):
         #calculation nonlocal part of Hamiltonian
