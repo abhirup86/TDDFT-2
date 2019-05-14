0ccd096a28066e51c978ca4361b83da3d878490f
yaros72 <yaroslav.zhumagulov@gmail.com>
2019-05-14 14:31:10 +0300

    nonlocal current update

--- RT-VG/TimeDependentHamiltonian.py
+++ RT-VG/TimeDependentHamiltonian.py
@@ -3,49 +3,7 @@ from gpaw.wavefunctions.pw import PWLFC
 from gpaw.utilities import unpack
 from gpaw.transformers import Transformer
 from itertools import product
-import numba
-
-    
-
-@numba.jit(nopython=True,parallel=True,fastmath=True)
-def fast_local(matrix,V,psi):
-    psi_conj=psi.conj()
-    nq=matrix.shape[0]
-    nbands=matrix.shape[1]
-    for q in numba.prange(nq):
-        for n in range(nbands):
-            for m in range(nbands):
-                matrix[q,n,m]=np.sum(psi_conj[q,n]*V*psi[q,m])
-    return matrix
-
-@numba.jit(nopython=True,parallel=True,fastmath=True)
-def fast_nonlocal(matrix,proj,
-                  V,psi,chi,phase,norm):
-    nq=psi.shape[0]
-    nbands=psi.shape[1]
-    norbs=chi.shape[1]
-    chi_conj=chi.conj()
-    for q in numba.prange(nq):
-        for n in range(nbands):
-            for o in range(norbs):
-                proj[q,n,o]=np.sum(chi_conj[q,o]*psi[q,n]*phase)*norm
-    proj_conj=proj.conj()
-    for q in numba.prange(nq):      
-        for n in range(nbands):
-            for m in range(nbands):
-                for o in range(norbs):
-                    matrix[q,n,m]+=V[o]*proj_conj[q,n,o]*proj[q,m,o]
-    return matrix
-
-@numba.jit(nopython=True,parallel=True,fastmath=True)
-def fast_density(matrix,f_n,wfn,den):
-    nq=den.shape[0]
-    nbands=den.shape[1]
-    for q in numba.prange(nq):
-        for n in range(nbands):
-            for m in range(nbands):
-                matrix+=f_n[q,n]*np.abs(wfn[q,n,m])**2*den[q,m]
-    return matrix
+from fast_functions import *
 
 class TimeDependentHamiltonian():
     
@@ -61,6 +19,7 @@ class TimeDependentHamiltonian():
         #initialization plane and grid descriptors from GPAW calculation
         self.pd=calc.wfs.pd
         self.gd=calc.wfs.gd
+        self.volume=np.abs(np.linalg.det(self.gd.cell_cv)) 
         
         #number of k-points
         self.nq=len(calc.wfs.kpt_u)
@@ -74,9 +33,9 @@ class TimeDependentHamiltonian():
         #overlap operator
         self.overlap=np.zeros((self.nq,self.nbands,self.nbands),dtype=complex)
         #local momentum operator
-        self.local_momentum=np.zeros((3,self.nq,self.nbands,self.nbands),dtype=complex)
+        self.local_moment=np.zeros((3,self.nq,self.nbands,self.nbands),dtype=complex)
         #nonlocal momentum operator
-        self.nonlocal_momentum=np.zeros((3,self.nq,self.nbands,self.nbands),dtype=complex)
+        self.nonlocal_moment=np.zeros((3,self.nq,self.nbands,self.nbands),dtype=complex)
         #Fermi-Dirac occupation
         self.f_n=np.zeros((self.nq,self.nbands),dtype=float)
         
@@ -84,15 +43,12 @@ class TimeDependentHamiltonian():
         psi_gs=[]
         #ground state Kohn-Sham orbitals density
         den_gs=[]
-        
-        
+                
         for kpt in self.wfs.kpt_u:
             self.overlap[kpt.q]=np.eye(self.nbands)
             self.f_n[kpt.q]=kpt.f_n
-            
             kinetic=0.5 * self.pd.G2_qG[kpt.q] # |G+q|^2/2
             gradient=self.pd.get_reciprocal_vectors(kpt.q)
-            
             psi=[];den=[]
             for n in range(self.nbands):
                 psi.append(self.pd.ifft(kpt.psit_nG[n],kpt.q))
@@ -102,7 +58,7 @@ class TimeDependentHamiltonian():
                     #calculation local momentum
                     #<psi_qn|\nabla|psi_qm>
                     for i in range(3):
-                        self.local_momentum[i,kpt.q,n,m]=self.pd.integrate(kpt.psit_nG[n],gradient[:,i]*kpt.psit_nG[m])
+                        self.local_moment[i,kpt.q,n,m]=self.pd.integrate(kpt.psit_nG[n],gradient[:,i]*kpt.psit_nG[m])
             psi_gs.append(psi)
             den_gs.append(den)    
             
@@ -115,29 +71,9 @@ class TimeDependentHamiltonian():
         #initialization local and nonlocal part of pseudopotential
         self.init_potential()
         
+        self.proj=np.zeros((self.nq,self.nbands,self.norb),dtype=complex)
+        self.proj_r=np.zeros((3,self.nq,self.nbands,self.norb),dtype=complex)
         
-        #calculation nonlocal momentum operator
-        # [r,V_NL]=sum_o V_o (r|chi_o><chi_o - |chi_o><chi_o|r)
-        proj=np.zeros((self.nq,self.nbands,self.norb),dtype=complex)
-        proj_r=np.zeros((3,self.nq,self.nbands,self.norb),dtype=complex)
-        
-        for q in range(self.nq):
-            for n in range(self.nbands):
-                for o in range(self.norb):
-                    # <psi_qn|chi_qo>
-                    proj[q,n,o]=calc.wfs.gd.integrate(self.psi_gs[q,n],self.chi[q,o])
-                     # <psi_qn|r|chi_qo>
-                    for i in range(3):
-                        proj_r[i,q,n,o]=calc.wfs.gd.integrate(self.psi_gs[q,n],self.r[i]*self.chi[q,o])
-            for n in range(self.nbands):
-                for m in range(self.nbands):
-                    for o in range(self.norb):
-                        for i in range(3):
-                            #<psi_qn|[r,V_NL]|psi_qm>=
-                            #= sum_o V_o (<psi_qn|r|chi_qo><chi_qo|psi_qm>-<psi_qn|chi_qo><chi_qo|r|psi_qm>)
-                            self.nonlocal_momentum[i,q,n,m]+=self.V[o]*proj_r[i,q,n,o]*proj[q,m,o].conj()
-                            self.nonlocal_momentum[i,q,n,m]-=self.V[o]*proj[q,n,o]*proj_r[i,q,m,o].conj()
-        self.momentum=self.local_momentum+self.nonlocal_momentum
         self.density=self.den.nt_sG.copy()
         
         #initialization charge density (ion+electrons) for Hartree potential
@@ -154,7 +90,65 @@ class TimeDependentHamiltonian():
         
         #initialization local potenital from ground state density
         self.update_local_potential()
-        self.VNL0=self.calculate_nonlocal(A=[0,0,0])
+        self.update_gauge([0,0,0])
+#----------------------------------------------------------------------------------------------------          
+    def init_potential(self):
+        #initialization of nonlocal part of pseudopotential
+        # V_NL=|chi_i> V_i <chi_i|
+        spline_aj = []
+        for setup in self.wfs.setups:
+            spline_aj.append(setup.pt_j)
+        self.lfc=PWLFC(spline_aj,self.pd)
+        self.lfc.set_positions(self.calc.spos_ac)  #set position of atoms
+        proj_G=[];proj_r=[] #collect chi_i in real space using FFT
+        
+        for kpt in self.wfs.kpt_u:
+            proj_G.append(self.lfc.expand(kpt.q))
+            proj=[];n_i=proj_G[-1].shape[1]
+            for i in range(n_i):
+                proj.append(self.pd.ifft(proj_G[-1][:,i].copy(),kpt.q))
+            proj_r.append(proj)
+        self.chi=np.array(proj_r)/self.gd.dv
+        
+        s=0 # s=0 because we perform spin-paired calculation
+        V=[] #collect V 
+        for a in range(len(self.wfs.setups)):
+            dH_ii = unpack(self.ham.dH_asp[a][s])
+            V.append(dH_ii.diagonal())
+            
+        V=np.array(V);
+        self.V=V.ravel()
+        
+        self.norb=self.V.size # number of orbitals in nonlocal potential
+        
+        #initialization of local part of pseudopotential
+        V=self.ham.vbar.pd.zeros()
+        self.ham.vbar.add(V)
+        self.Vloc=self.ham.vbar.pd.ifft(V)
+        
+#----------------------------------------------------------------------------------------------------          
+    def update_density(self,wfn):
+        self.density[0]=np.zeros_like(self.density[0])
+        fast_density(self.density[0],self.f_n,wfn,self.den_gs)
+        self.occupation=np.einsum('qnm,qn->qm',np.abs(wfn)**2,self.f_n)                
+        
+#---------------------------------------------------------------------------------------------------- 
+    def update_gauge(self,A):
+        #update projections p_qno=<chi_qo|psi_qn>
+        phase=np.exp(1j*np.einsum('ixyz,i->xyz',self.r,A))
+        fast_projections(self.proj,self.chi,phase,self.psi_gs,self.gd.dv)
+        
+        #update interaction hamiltonian
+        #H_I=\nabla*A+0.5*A^2
+        self.interaction=np.einsum('i,iqnm->qnm',A,self.local_moment)+0.5*np.linalg.norm(A)**2*self.overlap
+        
+        #calculation nonlocal momentum operator
+        # I_NL=[r,V_NL]=sum_o V_o (r|chi_o><chi_o - |chi_o><chi_o|r)
+        for i in range(3):
+            fast_projections(self.proj_r[0],self.chi,self.r[i]*phase,self.psi_gs,self.gd.dv)
+        self.nonlocal_moment =-1j*(np.einsum('o,iqno,qmo->iqnm',self.V,self.proj_r.conj(),self.proj)-
+                                   np.einsum('o,qno,iqmo->iqnm',self.V,self.proj.conj(),self.proj_r))
+        self.moment=self.local_moment+self.nonlocal_moment
         
 #----------------------------------------------------------------------------------------------------          
     def update_local_potential(self):
@@ -174,99 +168,31 @@ class TimeDependentHamiltonian():
         #transform Hartree and XC potential from fine to coarse grids
         self.VXC=self.fine_to_coarse.apply(VXC[0])
         self.VH=self.fine_to_coarse.apply(VH)
-            
-#----------------------------------------------------------------------------------------------------          
-    def init_potential(self):
-        #initialization of nonlocal part of pseudopotential
-        # V_NL=|chi_i> V_i <chi_i|
-        spline_aj = []
         
-        for setup in self.wfs.setups:
-            spline_aj.append(setup.pt_j)
-            
-        self.lfc=PWLFC(spline_aj,self.pd)
-       
-        self.lfc.set_positions(self.calc.spos_ac)  #set position of atoms
-        
-        proj_G=[];proj_r=[] #collect chi_i in real space using FFT
-        
-        for kpt in self.wfs.kpt_u:
-            proj_G.append(self.lfc.expand(kpt.q))
-            proj=[];n_i=proj_G[-1].shape[1]
-            
-            for i in range(n_i):
-                proj.append(self.pd.ifft(proj_G[-1][:,i].copy(),kpt.q))
-                
-            proj_r.append(proj)
-            
-        self.chi=np.array(proj_r)/self.gd.dv
-        
-        
-        s=0 # s=0 because we perform spin-paired calculation
-        V=[] #collect V 
-        for a in range(len(self.wfs.setups)):
-            dH_ii = unpack(self.ham.dH_asp[a][s])
-            V.append(dH_ii.diagonal())
-            
-        V=np.array(V);
-        self.V=V.ravel()
-        
-        self.norb=self.V.size # number of orbitals in nonlocal potential
-        
-        #initialization of local part of pseudopotential
-        V=self.ham.vbar.pd.zeros()
-        self.ham.vbar.add(V)
-        self.Vloc=self.ham.vbar.pd.ifft(V)
-#----------------------------------------------------------------------------------------------------          
-    def update_density(self,wfn):
-        self.density[0]=fast_density(np.zeros_like(self.density[0]),self.f_n,wfn,self.den_gs)
-        self.occupation=np.einsum('qnm,qn->qm',np.abs(wfn)**2,self.f_n)
-        
-#----------------------------------------------------------------------------------------------------    
-    def calculate_nonlocal(self,A):
+#----------------------------------------------------------------------------------------------------            
+    def calculate_nonlocal(self):      
         #calculation nonlocal part of Hamiltonian
-        #with gauge transform according to vector potential A
-        phase=np.exp(1j*np.einsum('ixyz,i->xyz',self.r,A))
-        VNL=np.zeros((self.nq,self.nbands,self.nbands),dtype=complex)
-        proj=np.zeros((self.nq,self.nbands,self.norb),dtype=complex)
-        VNL=fast_nonlocal(VNL,proj,self.V,self.psi_gs,self.chi,phase,self.gd.dv)                    
-        return VNL
+        return np.einsum('o,qno,qmo->qnm',self.V,self.proj.conj(),self.proj)
+    
 #----------------------------------------------------------------------------------------------------      
     def calculate_local(self):
-        VL=np.zeros((self.nq,self.nbands,self.nbands),dtype=complex)
-        potential=self.Vloc+self.VXC+self.VH
-        VL=fast_local(VL,potential,self.psi_gs)
-        VL*=self.gd.dv
-        return VL
+        #calculation local part of Hamiltonian
+        local=np.zeros((self.nq,self.nbands,self.nbands),dtype=complex)
+        return fast_local(local,self.Vloc+self.VXC+self.VH,self.psi_gs,self.gd.dv)
+    
 #----------------------------------------------------------------------------------------------------      
     def calculate_kinetic(self):
         #calculation kinetic part of Hamiltonian
         return self.kinetic
+    
 #----------------------------------------------------------------------------------------------------      
-    def calculate_interaction(self,A):
-        #calculation interaction with field part
-        return np.einsum('i,iqnm->qnm',A,self.local_momentum)+0.5*np.linalg.norm(A)**2*self.overlap
-#----------------------------------------------------------------------------------------------------      
-    def hamiltonian(self,A=[0,0,0]):
-        #Hamiltonian calculation 
-        K=self.calculate_kinetic()
-        VL=self.calculate_local()
-        if A==0:
-            return K+VL+self.VNL0
-        else:
-            VNL=self.calculate_nonlocal(A)
-            I=self.calculate_interaction(A)
-            return K+VL+VNL+I
+    def hamiltonian(self):
+        #calculation hamiltonian
+        return self.calculate_kinetic()+self.calculate_local()+self.calculate_nonlocal()+self.interaction
+    
 #----------------------------------------------------------------------------------------------------    
     def calculate_current(self,wfn):
-#         VNL=self.calculate_nonlocal(A)
-#         J=np.zeros((3,self.nq,self.nbands,self.nbands),dtype=complex)
-#         for i in range(3):
-#             for q in range(self.nq):
-#                 J[i,q]=np.dot(VNL[q],self.dipole[i,q])-np.dot(self.dipole[i,q],VNL[q])
-        current=np.einsum('iqnm,qb,qnb,qmb->i',self.momentum,self.f_n,wfn.conj(),wfn)
-#         current+=self.nelectrons*A
-        return current
+        return np.einsum('iqnm,qb,qnb,qmb->i',self.moment,self.f_n,wfn.conj(),wfn)/self.volume
         
         
        
--- RT-VG/TimeDependentPropagator.py
+++ RT-VG/TimeDependentPropagator.py
@@ -10,43 +10,45 @@ def iteration(nq,dt,H,I,wfn,wfn_next):
         H_right=I-0.5j*dt*H[q]
         wfn_next[q]=solve(H_left, np.dot(H_right,wfn[q]))
     return wfn_next
+
 def smooth(t,dt,steps):
     return 1.-3.*(t/(steps*dt))**2+2.*(t/(steps*dt))**3
 
 
 class TimeDependentPropagator():
     def __init__(self,TDH):
-        self.volume=np.abs(np.linalg.det(TDH.wfs.gd.cell_cv)) 
         self.TDH=TDH
         self.nq=TDH.nq
         self.nbands=TDH.nbands
         
     def linear_response(self,dt,steps,A0=[1e-5,0,0],NSCsteps=3):
         
-        self.A0=A0
         self.J=np.zeros((steps,3),dtype=np.complex)
-        
         I=np.eye(self.nbands)
-        H=self.TDH.hamiltonian(A0)
-        
+
+        #calculation start wavefunctions
         wfn=np.zeros((self.nq,self.nbands,self.nbands),dtype=np.complex)
-        
+        self.TDH.update_gauge(A0)
+        H=self.TDH.hamiltonian()
         for q in range(self.nq):
             E,D=np.linalg.eigh(H[q])
             wfn[q]=D
-         
+        self.TDH.update_gauge([0,0,0])
+        H=self.TDH.hamiltonian()
+        
+        #time propagation
         for t in tqdm(range(steps)):
             self.J[t]=self.TDH.calculate_current(wfn)
             wfn_next=np.copy(wfn)
             for i in range(NSCsteps):
                 self.TDH.update_density(wfn_next)
-                H_next = self.TDH.hamiltonian(0)
+                H_next = self.TDH.hamiltonian()
                 H_mid = 0.5*(H + H_next)
                 wfn_next=iteration(self.nq,dt,H_mid,I,wfn,wfn_next)
             wfn=np.copy(wfn_next)            
             H = np.copy(H_next)
-            
-        self.J=self.J/self.volume
+        
+        #linear response calculation
         time=np.arange(steps)*dt
         freq = np.fft.fftfreq(steps, d=dt)
         freq=np.sort(freq)
@@ -63,30 +65,31 @@ class TimeDependentPropagator():
         return epsilon,freq
         
     def propagate(self,A,steps,dt,NSCsteps=3):
+        #time-dependent propagation
+        # A - function A(t)
+        # steps - number of time steps
+        # dt - step in a.u. time
+        # NSCsteps - number of corrections
         
         self.J=np.zeros((steps,3),dtype=complex)
         wfn=np.zeros((self.nq,self.nbands,self.nbands),dtype=complex)
-        for q in range(self.nq):
-            wfn[q]=np.eye(self.nbands)
+        for q in range(self.nq): wfn[q]=np.eye(self.nbands)
             
         self.TDH.update_density(wfn)
         H = self.TDH.hamiltonian()
-        I=np.eye(self.nbands)
+        I = np.eye(self.nbands)
         
         for t in tqdm(range(steps)):
+            self.J[t]=self.TDH.calculate_current(wfn)
+            self.TDH.update_gauge(A(t*dt))
             wfn_next=np.copy(wfn)
             for i in range(NSCsteps):
-                
                 self.TDH.update_density(wfn_next)
-                H_next = self.TDH.hamiltonian(A(t*dt))
+                H_next = self.TDH.hamiltonian()
                 H_mid = 0.5*(H + H_next)
                 wfn_next=iteration(self.nq,dt,H_mid,I,wfn,wfn_next)
-                
             wfn=np.copy(wfn_next)            
             H = np.copy(H_next)
-            self.J[t]=self.TDH.calculate_current(wfn)
-            
-        self.wfn=wfn
         
                                              
             
